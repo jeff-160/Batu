3180f3b8dd9f78915c72eccd89f868281e2533a6
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-01-14 12:25:14 +0800

    Add files via upload

--- config.bat
+++ config.bat
@@ -11,6 +11,20 @@ if %errorlevel% neq 0 (
     exit /b
 )
 
+
+powershell -command Write-Host Installing VSCode extensions -ForeGroundColor Yellow
+set "dest=%USERPROFILE%\\.vscode\\extensions"
+set "dir=batu-syntax-highlighting"
+if EXIST "%dest%" (
+    xcopy "%cd%\\%dir%" "%dest%\\%dir%" /E /I /Y > nul
+    powershell -command Write-Host VSCode extensions installed -ForegroundColor Green
+) else (
+    powershell -command Write-Host VSCode extensions path not found -ForegroundColor Red
+    goto :end
+)
+goto :end
+
+
 powershell -command Write-Host Adding path to system environment variables -ForeGroundColor Yellow
 setx /m PATH "%PATH%;%cd%"
 if %errorlevel% neq 0 (
@@ -20,18 +34,18 @@ if %errorlevel% neq 0 (
     powershell -command Write-Host ^"%cd%^" added to system environment variables -ForeGroundColor Green
 )
 
-@REM set "ext=.batu"
-@REM set "icon=%cd%\batu.ico"
 
-@REM assoc %ext% >nul 2>&1
-@REM if errorlevel 1 (
-@REM     assoc %ext%=filetype
-@REM     ftype filetype=%icon%
+set "ext=.batu"
+set "icon=%cd%\batu.ico"
+assoc %ext% >nul 2>&1
+if errorlevel 1 (
+    assoc %ext%=filetype
+    ftype filetype=%icon%
+    taskkill /IM explorer.exe /F >nul
+    del /f /s /q "%userprofile%\AppData\Local\IconCache.db" >nul
+    start explorer.exe
+)
 
-@REM     taskkill /IM explorer.exe /F >nul
-@REM     del /f /s /q "%userprofile%\AppData\Local\IconCache.db" >nul
-@REM     start explorer.exe
-@REM )
 
 :end
 echo.
