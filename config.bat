212e1f1eaba66bd5c13d5cb56fd4cf7549c81a72
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-01-14 17:07:41 +0800

    Update config.bat

--- config.bat
+++ config.bat
@@ -1,5 +1,14 @@
 @echo off
 
+goto :main
+
+:print
+powershell -command "Write-host %1 -ForegroundColor %2"
+exit /b
+
+
+:main
+
 cd /d %~dp0
 
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
@@ -12,31 +21,39 @@ if %errorlevel% neq 0 (
 )
 
 
-powershell -command Write-Host Installing VSCode extensions -ForeGroundColor Yellow
+set "batu=%USERPROFILE%\\Batu"
+xcopy %cd% "%batu%" /E /I /Y > nul
+cd /d "%batu%"
+
+
+call :print "Installing VSCode extensions" Yellow
 set "dest=%USERPROFILE%\\.vscode\\extensions"
 set "dir=batu-syntax-highlighting"
-if EXIST "%dest%" (
+if exist "%dest%" (
     xcopy "%cd%\\%dir%" "%dest%\\%dir%" /E /I /Y > nul
-    powershell -command Write-Host VSCode extensions installed -ForegroundColor Green
+    call :print "VSCode extensions installed" Green
 ) else (
-    powershell -command Write-Host VSCode extensions path not found -ForegroundColor Red
+    call :print "VSCode extensions path not found" Red
     goto :end
 )
+echo.
+
+
+call :print "Adding path to system environment variables" Yellow
+setx /m PATH "%PATH%;%cd%"
+if %errorlevel% neq 0 (
+    call :print "Failed to add %cd% to system environment variables" Red
+    goto :end
+) else (
+    call :print "%cd% added to system environment variables" Green
+)
+echo.
 goto :end
 
 
-powershell -command Write-Host Adding path to system environment variables -ForeGroundColor Yellow
-setx /m PATH "%PATH%;%cd%"
-if %errorlevel% neq 0 (
-    powershell -command Write-Host Failed to add ^"%cd%^" to system environment variables -ForegroundColor Red
-    goto :end
-) else (
-    powershell -command Write-Host ^"%cd%^" added to system environment variables -ForeGroundColor Green
-)
-
-
+call :print "Associating language icon" Yellow
 set "ext=.batu"
-set "icon=%cd%\batu.ico"
+set "icon=%cd%\resources\batu.ico"
 assoc %ext% >nul 2>&1
 if errorlevel 1 (
     assoc %ext%=filetype
@@ -44,6 +61,10 @@ if errorlevel 1 (
     taskkill /IM explorer.exe /F >nul
     del /f /s /q "%userprofile%\AppData\Local\IconCache.db" >nul
     start explorer.exe
+
+    call :print "Icon associated with .batu extension"
+) else (
+    call :print "Failed to associate icon"
 )
 
 
